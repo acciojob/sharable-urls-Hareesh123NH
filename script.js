38dff86fb23298c86353b40e364c01b09f6cd958
Hareesh123NH <162862005+Hareesh123NH@users.noreply.github.com>
2024-04-27 18:40:13 +0530

    created a submission

--- script.js
+++ script.js
@@ -3,7 +3,17 @@ document.getElementById('button').addEventListener('click', function() {
     var name = document.getElementById('name').value;
     var year = document.getElementById('year').value;
     
-    var url = "https://localhost:8080/?name=" + encodeURIComponent(name) + "&year=" + encodeURIComponent(year);
-    
-    document.getElementById('url').innerText = "URL: " + url;
+    var url = "https://localhost:8080/"+"";
+    if (name!="" || year!="") {
+    	if (name!="" && year!="") {
+    		url=url+"?name="+name+"&year="+year;
+    	} 
+		else if(name!=""){
+			url=url+"?name="+name;
+		}
+		else {
+    		url=url+"?year="+year;
+    	}
+    }
+    document.getElementById('url').innerText = url;
 });
